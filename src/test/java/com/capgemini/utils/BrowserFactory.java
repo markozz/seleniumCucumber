2bcf3296c6bb961ecbd5027a5b3f7424f2883fe8
mark <mpabrahams@gmail.com>
2016-12-06 14:47:15 +0100

    changed location of feature files, added properties file that contains what browser should be started to run the tests on

--- src/test/java/com/capgemini/utils/BrowserFactory.java
+++ src/test/java/com/capgemini/utils/BrowserFactory.java
@@ -6,6 +6,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class BrowserFactory {
     private static WebDriver browser;
     private static boolean running = false;
@@ -31,11 +36,38 @@ public class BrowserFactory {
     }
 
     private void beforeAll() {
-//        System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir") + "\\geckodriver.exe");
-//        browser = new FirefoxDriver();
+        Properties properties = new Properties();
+        String browser = "";
+        try {
+            FileInputStream propertiesFile = new FileInputStream("src/test/resources/com/capgemini/config/config.properties");
+            properties.load(propertiesFile);
+            browser = properties.getProperty("browser");
+        } catch (FileNotFoundException e) {
+            System.out.println("WARN: could not find config file, using default browser (Chrome)");
+            browser = "chrome";
 
-        System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
-        browser = new ChromeDriver();
+        } catch (IOException e) {
+            System.out.println("WARN: could not load properties file, using default browser (Chrome)");
+            browser = "chrome";
+        } finally {
+            switch (browser) {
+                case "chrome":
+                    System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+                    this.browser = new ChromeDriver();
+                    break;
+                case "firefox":
+                    System.setProperty("webdriver.gecko.driver", "./geckodriver.exe");
+                    this.browser = new FirefoxDriver();
+                    break;
+                default:
+                    System.out.println("WARN: Browser specified in properties file is unsupported, using default (Chrome)");
+                    System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+                    this.browser = new ChromeDriver();
+                    break;
+            }
+        }
+
+
     }
 
     private void afterAll() {
